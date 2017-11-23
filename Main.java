09842c4f6c89613775573ac3fb43122d9723b0e1
Jacob Margolis <jmargolis@live.com>
2017-11-22 19:58:59 -0500

    Added generateMap()
    Tried to get generateMap() to work. Note also added displayMap(), which works quite fine.

--- Main.java
+++ Main.java
@@ -1,14 +1,18 @@
-public static class Main{
+public class Main{
+  static char[][] map = null;
+  static int posx, posy;
+  static int oldPosx, oldPosy;
+
   public static void main(String[] args){
     generateMap();
-    while(!dead){
+    //while(!dead){
       displayMap();
-      move();
-    }
+      //move();
+    //}
   }
-
+/*
   public static void move(){
-    pos = readInput();
+    readInput(); //assigns posx and posy
     char next = readAhead();
     updateMap(posx, posy, ‘X’); //or whatever we want the player to be /
     updateMap(oldPosx, oldPosy, next);
@@ -17,8 +21,44 @@ public static class Main{
     oldPos=pos;
     updateDead();
   }
+*/
+
+  public static void generateMap(){
+    map  =  {
+              {  '-',  '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-' },
+              {  '-',  '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-' },
+              {  '-',  '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-' },
+              {  '-',  '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-' },
+              {  '-',  '-', '-', '-', '-', '-', 'X', '-', '-', '-', '-', '-', '-' },
+              {  '-',  '-', 'E', '-', '-', '-', 'X', 'X', '-', '-', '-', '-', '-' },
+              {  '-',  '-', '-', '-', '-', 'X', 'X', 'X', 'X', 'X', '-', '-', '-' },
+              {  '-',  '-', '-', '-', '-', '-', 'X', 'X', 'X', 'X', 'X', '-', '-' },
+              {  '-',  '-', '-', '-', '-', '-', '-', 'X', 'X', 'X', 'X', '-', '-' },
+              {  '-',  '-', '-', '-', '-', '-', '-', '-', 'X', 'X', 'X', '-', '-' },
+              {  '-',  '-', '-', '-', '-', '-', '-', '-', 'X', 'X', 'X', '-', '-' },
+              {  '-',  '-', '-', 'X', '-', '-', '-', '-', '-', 'X', '-', '-', '-' },
+              {  '-',  '-', 'X', 'X', '-', '-', 'P', '-', '-', '-', '-', '-', '-' },
+              {  '-',  'X', 'X', 'X', 'X', '-', '-', '-', '-', '-', '-', '-', '-' },
+              {  '-',  '-', 'X', 'X', 'X', '-', '-', '-', '-', '-', '-', '-', '-' },
+              {  '-',  '-', '-', 'X', 'X', 'X', '-', '-', '-', '-', '-', '-', '-' },
+              {  '-',  '-', '-', '-', 'X', 'X', '-', '-', '-', '-', '-', '-', '-' },
+              {  '-',  '-', '-', '-', '-', 'X', 'X', '-', '-', '-', '-', '-', '-' },
+              {  '-',  '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', 'E', '-' },
+              {  '-',  '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-' },
+              {  '-',  '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-' },
+            };
+  }
 
   public static void updateMap(int posx, int posy, char c){
     map[posx][posy] = c;
+  }
+
+  public static void displayMap(){
+    for (char[] mapRow : map){
+      for (char i : mapRow){
+        System.out.print(i);
+      }
+      System.out.print("\n");
+    }
   }
 }
