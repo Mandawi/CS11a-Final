16d9c2f22560a1e7e5e875a92a884ae299a48e4a
Jacob Margolis <jmargolis@live.com>
2017-11-27 17:37:02 -0500

    added readAhead() and some javadoc
    Added the readAhead() class, which reads the character at the given location in a 2d char array (it will always be given the current map)
    
    Also added some sporadic javadoc and got rid of some pseudocode that we don't need

--- Main.java
+++ Main.java
@@ -16,9 +16,7 @@ public class Main{
     updateMap(posx, posy, ‘X’); //or whatever we want the player to be /
     updateMap(oldPosx, oldPosy, next);
     runEvent(next);
-    increment();
-    oldPos=pos;
-    updateDead();
+    oldPosx=posx; oldPosy = posy;
   }
 
 
@@ -49,11 +47,23 @@ public class Main{
     return map;
   }
 
+  /**
+  * changes the character at the given position on the given 2d char array into the given character
+  * @param posx x position of the character to be changed
+  * @param posy y position of the character to be changed
+  * @param c character to be changed to
+  * @param map 2d char array that is being changed
+  * @return The 2d char array with the changes
+  */
   public static char[][] updateMap(int posx, int posy, char c, char[][] map){
     map[posx][posy] = c;
     return map;
   }
 
+  /**
+  * prints out a 2d array of characters
+  * @param map the 2d array to be printed
+  */
   public static void displayMap(char[][] map){
     for (char[] mapRow : map){
       for (char i : mapRow){
@@ -61,5 +71,16 @@ public class Main{
       }
       System.out.print("\n");
     }
+  }
+
+  /**
+  * returns the character of the given location on the array
+  * @param posx x position of the character to be looked at at
+  * @param posy y position of the character to be changed
+  * @param map 2d char array that is looked at
+  * @return The character at that position of the 2d char array
+  */
+  public static char readAhead(int pox, int posy, char[][] map){
+    return map[posx][posy];
   }
 }
