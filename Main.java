f87bdae563bac9b3e0a251080c2cc12adee6a44d
Jacob Margolis <jmargolis@live.com>
2017-12-03 13:29:46 -0500

    enemy movement incomplete
    still doesn't work properly but pushing so we get some idea

--- Main.java
+++ Main.java
@@ -1,24 +1,36 @@
+import java.util.*;
+import java.time.Instant;
+import java.lang.*;
+
 public class Main{
   static int posx, posy;
   static int oldPosx, oldPosy;
-  static int[][][] enemies = new int[3][2];  //3 enemies, each with an x and y pos
+  static Boolean dead = false;
+  static int[][] enemies = {
+      {5, 2},
+      {19 ,18},
+      {9 ,20}
+    };  //3 enemies, each with an x and y pos, this is the first map
 
   public static void main(String[] args){
+    char[][] map = generateMap(1);
     char map[][] = generateMap(1);//we can have the menu input which map here somehow
     while(!dead){
       displayMap(map);
-      movePlayer();
+      movePlayer(map);
+      map = moveEnemies(map);
     }
   }
 
+/*
   public static void movePlayer(){
     readInput(); //assigns posx and posy
     char next = readAhead();
-    updateMap(posx, posy, ‘X’); //or whatever we want the player to be
+    updateMap(posx, posy, 'X'); //or whatever we want the player to be
     updateMap(oldPosx, oldPosy, next);
     runEvent(next);
     oldPosx=posx; oldPosy = posy;
-  }
+  }*/
 
 
   /**
@@ -28,7 +40,7 @@ public class Main{
   */
   public static char[][] generateMap(int which){
     if (which == 1){
-      char[][] map  =  {
+      char[][] map  =  new char[][]{
                 {  '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_' },
                 {  '|', '-',  '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-','-','-','-','-','-','-', '|' },
                 {  '|', '-',  '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-','-','-','-','-','-','-', '|'},
@@ -53,8 +65,9 @@ public class Main{
                 {  '|', '-',  '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-','-','-','-','-','-','-', '|'},
                 {  '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_' }
               }; //23 x  23
+      return map;
     } else if (which == 2){
-        char[][] map  =  {
+        char[][] map  =  new char[][]{
                 {  '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_' },
                 {  '|', '-',  '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-','-','-','-','-','-','-', '|' },
                 {  '|', '-',  '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-','-','-','-','-','-','-', '|'},
@@ -79,8 +92,9 @@ public class Main{
                 {  '|', '-',  '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-','-','-','-','-','-','-', '|'},
                 {  '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_' }
               };
+      return map;
     } else {
-        char[][] map  =  {
+        char[][] map  =  new char[][]{
                   {  '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_' },
                   {  '|', '-',  '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', 'X','-','-','-','-','-','-', '|' },
                   {  '|', '-',  '-', '-', '-', 'X', '-', '-', '-', '-', '-', '-', '-', '-', 'X', 'X','-','-','-','-','-','-', '|'},
@@ -105,8 +119,8 @@ public class Main{
                   {  '|', '-',  'X', 'X', 'X', 'X', 'X', '-', '-', '-', '-', '-', '-', '-', '-', '-','-','-','-','-','-','-', '|'},
                   {  '_', '_', 'X', 'X', 'X', 'X', 'X', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_' }
                 };
+        return map;
       }
-    return map;
   }
 
   /**
@@ -146,7 +160,104 @@ public class Main{
     return map[posx][posy];
   }
 
+  /**
+  * reads the given direction on the 2d array for the given enemy
+  * @param direction the string of the direction
+  * @param map the 2d array
+  * @param i the enemy
+  * @return the char of what is at that spot
+  */
+  public static char readDirectionEnemies(String direction, char[][] map, int i){
+    if (direction.equals("left")){
+      return readAhead(enemies[i][0], enemies[i][1]-1, map);
+    } else if (direction.equals("right")){
+      return readAhead(enemies[i][0], enemies[i][1]+1, map);
+    } else if (direction.equals("up")){
+      return readAhead(enemies[i][0]-1, enemies[i][1], map);
+    } else {
+      return readAhead(enemies[i][0]+1, enemies[i][1], map);
+    }
+  }
 
+  /**
+  * Runs operations so that all the (3) enemies on the map move forward in a random directions
+  * @param map the map array
+  * @return the map string
+  */
+  public static char[][] moveEnemies(char[][] map){
+    for(int i = 0; i < enemies.length; i++){
+      try
+      {
+        Thread.sleep(1000);
+      }
+      catch(InterruptedException e)
+      {
+        // this part is executed when an exception (in this example InterruptedException) occurs
+      }
+      String direction = getDirection();
+      char ahead = readDirectionEnemies(direction, map, i);
+      while(ahead == 'X' || ahead == '|' || ahead == '_'){
+        try
+        {
+          Thread.sleep(1000);
+        }
+        catch(InterruptedException e)
+        {
+          // this part is executed when an exception (in this example InterruptedException) occurs
+        }
+        direction = getDirection();
+        ahead = readDirectionEnemies(direction, map, i);
+      }
 
+      map = replaceEnemies(direction, map, i);
+    }
+    return map;
+  }
 
+  /**
+  * actually moves the enemies on the map
+  * @param direction the string of the direction the enemy is moving
+  * @param map the map 2d array
+  * @param i which enemy is moving
+  * @return  the map array
+  */
+  public static char[][] replaceEnemies(String direction, char[][] map, int i){
+    if (direction.equals("left")){
+      updateMap(enemies[i][0], enemies[i][1]-1, 'E', map);
+      updateMap(enemies[i][0], enemies[i][1], '-', map);
+      enemies[i][1] = enemies[i][1] -1;
+    } else if (direction.equals("right")){
+      updateMap(enemies[i][0], enemies[i][1]+1, 'E', map);
+      updateMap(enemies[i][0], enemies[i][1], '-', map);
+      enemies[i][1] = enemies[i][1]+1;
+    } else if (direction.equals("up")){
+      updateMap(enemies[i][0]-1, enemies[i][1], 'E', map);
+      updateMap(enemies[i][0], enemies[i][1], '-', map);
+      enemies[i][0] = enemies[i][0]-1;
+    } else {
+      updateMap(enemies[i][0]+1, enemies[i][1], 'E', map);
+      updateMap(enemies[i][0], enemies[i][1], '-', map);
+      enemies[i][0] = enemies[i][0]+1;
+    }
+    return map;
+  }
+
+  /**
+  * gives a random direction - left, right, up, down
+  * @return the direction
+  */
+  public static String getDirection(){
+    Instant instant = Instant.now();
+    Random rand = new Random(instant.toEpochMilli());
+    int x = rand.nextInt(4) + 1;//for the 4 directions
+    if(x == 1){
+      return "left";
+    } else if (x == 2){
+      return "right";
+    } else if (x == 3){
+      return "up";
+    } else {
+      return "down";
+    }
+  }
 }
