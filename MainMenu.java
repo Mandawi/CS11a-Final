b900340ae7e8ef255f782d5f6a9f0a2dd343ff91
Osamah Mandawi <32464574+Mandawi@users.noreply.github.com>
2017-12-05 21:05:55 -0500

    Add more options and fix broken links

--- MainMenu.java
+++ MainMenu.java
@@ -10,24 +10,37 @@ public class MainMenu{
 		System.out.println("\\\\__________________________________//");
 		System.out.println("//__________________________________\\\\");
 		System.out.println("||Enter 1 to Start Game             ||");
-		System.out.println("||Enter 2 to Use A Different Map    ||");
-		System.out.println("||Enter 3 to Exit Game              ||");
+		System.out.println("||Enter 2 to See Instructions       ||");
+		System.out.println("||Enter 3 to Use A Different Map    ||");
+		System.out.println("||Enter 4 to See Credits            ||");
+		System.out.println("||Enter 5 to Exit Game              ||");
 		System.out.println("\\\\__________________________________//");
 		System.out.println("  |_________________________________|");
 		System.out.println("  |_________________________________|");
-		int crrntMp=0;
+		int crrntMp=1;
 		System.out.println("Current Map:"+crrntMp);  //shows what's the current map is
 		Scanner scn=new Scanner(System.in);
 		System.out.print("Input: "); //asks the user for input
 		int select=scn.nextInt();
 		Selector(select, crrntMp); //uses selector to decide action for input
 	}
+	public static void Instructions(){
+		String[] empty=new String[1];
+		System.out.println("You are controlling a ship indicated with an \"O\" using WASD.\nThe water body, which you can move through, is indicated with a \"-\".\nThe land, which you canNOT move through, is indicated with an \"X\".\nThe enemies, which you must chase and bump with to win, are indicated with an \"E\".\nHave fun!\nEnter 2 again to return to the main menu");
+		Scanner scn=new Scanner(System.in);
+		int in=scn.nextInt();
+		while (in!=2){
+			System.out.println("Enter 2");
+			in=scn.nextInt();
+		}
+		main(empty);
+	}
 	public static void Selector(int select, int crrntMp){
-		if (select==2){
-			if (crrntMp==0||crrntMp==1){
+		if (select==3){
+			if (crrntMp==1||crrntMp==2){
 				crrntMp+=1;
 			}
-			else if (crrntMp==2){
+			else if (crrntMp==3){
 				crrntMp-=2;
 			}
 			System.out.println("____________________________________");
@@ -38,17 +51,27 @@ public class MainMenu{
 			Selector(select, crrntMp);
 		}
 		else if (select==1){
-			//... starts game -- StartGame() class or whatever it is called
+			String[] empty=new String[1];
+			Main mainObject = new Main();
+			mainObject.main(empty);
 		}
-		else if (select==3){
+		else if (select==2){
+			Instructions();
+		}
+		else if (select==5){
+			System.out.println("Thank you for playing.");
 			System.exit(0);
+		}
+		else if (select==4){
+			System.out.println("\n\nCreated by Jacob, Osamah, Spencer, and Mark!\n\n");
+			String[] empty=new String[1];
+			main(empty);
 		}
 		else{
 			Scanner scn=new Scanner(System.in);
-			System.out.print("Re-enter input (Must be from 1-3): ");
+			System.out.print("Re-enter input (Must be from 1-5): ");
 			select=scn.nextInt();
 			Selector(select, crrntMp); //calls itself
 		}
-
 	}
 }
